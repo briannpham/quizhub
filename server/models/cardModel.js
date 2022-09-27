1d05314ec0a9a0a896fad08728871bad5fa30fd0
Brian Pham <brianpham@Brians-Mac-mini.home>
2022-09-27 11:37:23 -0700

    all route handlers works correctly and all potential errors are handled

--- server/models/cardModel.js
+++ server/models/cardModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const cardSchema = Schema({
   question: { type: String, required: true },
   answer: { type: String, required: true }
-});
+}, { timestamps: true });
 
 const Card = mongoose.model('card', cardSchema);
 
